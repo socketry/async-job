7c92dd36ac5803e7e4fe12eae34e92932391918a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-02-14 23:34:11 +1300

    Split `test.rb` -> `client.rb` and `server.rb` for easier testing.

--- client.rb
+++ client.rb
@@ -11,15 +11,6 @@ Async do
 	client = Async::Redis::Client.new
 	server = Async::Job::Backend::Redis::Server.new(client, "async:job")
 	
-	server_task = Async do
-		server.start
-		
-		server.each do |id, job|
-			Console.info(server, "Processing job: #{id} #{job.inspect}")
-			sleep(1)
-		end
-	end
-	
 	10.times do
 		job = Job.new(SecureRandom.uuid, Time.now.to_f, "Hello, World!")
 		Console.info(server, "Enqueuing job: #{job.id} #{job.inspect}")
---
+++ server.rb
@@ -0,0 +1,22 @@
+
+require 'async'
+require 'async/redis'
+require_relative 'lib/async/job'
+
+require 'securerandom'
+
+Job = Struct.new(:id, :perform_at, :serialize)
+
+Async do
+	client = Async::Redis::Client.new
+	server = Async::Job::Backend::Redis::Server.new(client, "async:job")
+	
+	server_task = Async do
+		server.start
+		
+		server.each do |id, job|
+			Console.info(server, "Processing job: #{id} #{job.inspect}")
+			sleep(1)
+		end
+	end
+end
