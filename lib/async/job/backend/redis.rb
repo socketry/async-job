e44e7c41b689f2417c34f2edf0336cb9fcd7f3c8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-02-25 03:18:31 +1300

    Minor fixes.

--- lib/async/job/backend/redis.rb
+++ lib/async/job/backend/redis.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
 require_relative 'redis/server'
+require 'async/redis/client'
 
 module Async
 	module Job
