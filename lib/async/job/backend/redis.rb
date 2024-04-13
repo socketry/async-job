9cf247b1ba2401f0b09c1ed63e0c7ec368c58c9a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-14 01:42:46 +1200

    Allow overriding protocol.

--- lib/async/job/backend/redis.rb
+++ lib/async/job/backend/redis.rb
@@ -10,8 +10,16 @@ module Async
 	module Job
 		module Backend
 			module Redis
-				def self.new(delegate, endpoint: Async::Redis.local_endpoint, client_class: Async::Redis::Client, **options)
-					client = client_class.new(endpoint)
+				# A simple server that processes jobs from a Redis backend.
+				#
+				# @parameter delegate [Object] The object that will be called with the job data.
+				# @parameter endpoint [Async::IO::Endpoint] The Redis endpoint to connect to.
+				# @parameter protocol [Async::Redis::Protocol] The Redis protocol to use.
+				# @parameter options [Hash] Additional options passed to the server.
+				def self.new(delegate, endpoint: Async::Redis.local_endpoint, protocol: nil, **options)
+					client_options = {protocol: protocol}.compact
+					
+					client = Async::Redis::Client.new(endpoint, **client_options)
 					return Server.new(delegate, client, **options)
 				end
 			end
