70eb20288f305f4daf81f9b6bab83bec2f9e5e86
Alexey <a.ivanov@3commas.io>
2024-07-31 12:17:29 +0400

    fix format

--- lib/async/job/generic.rb
+++ lib/async/job/generic.rb
@@ -4,27 +4,27 @@
 # Copyright, 2024, by Samuel Williams.
 
 module Async
-  module Job
-    class Generic
-      def self.enqueue(...)
-        self.new(...).enqueue
-      end
-
-      def initialize(id, scheduled_at: nil)
-        @id = id
-        @scheduled_at = scheduled_at
-      end
-
-      attr :id
-      attr :scheduled_at
-
-      def serialize
-        raise NotImplementedError
-      end
-
-      def call
-        raise NotImplementedError
-      end
-    end
-  end
+	module Job
+		class Generic
+			def self.enqueue(...)
+				self.new(...).enqueue
+			end
+			
+			def initialize(id, scheduled_at: nil)
+				@id = id
+				@scheduled_at = scheduled_at
+			end
+			
+			attr :id
+			attr :scheduled_at
+			
+			def serialize
+				raise NotImplementedError
+			end
+			
+			def call
+				raise NotImplementedError
+			end
+		end
+	end
 end
