7edb7f31d2482ab8ebc7cb21bd778be149debdbe
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-09 01:05:36 +1200

    Reorganise processor code + add delayed.

---
+++ lib/async/job/processor/delayed.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
+require_relative 'generic'
+
+require 'console/event/failure'
+
+module Async
+	module Job
+		module Processor
+			# Add a small processing delay to each job.
+			class Delayed < Generic
+				def initialize(delegate, delay: 0.1)
+					super(delegate)
+					
+					@delay = delay
+				end
+				
+				def call(job)
+					sleep(@delay)
+					
+					super
+				end
+			end
+		end
+	end
+end
--- lib/async/job/processor/inline.rb
+++ lib/async/job/processor/inline.rb
@@ -3,14 +3,41 @@
 # Released under the MIT License.
 # Copyright, 2024, by Samuel Williams.
 
-require_relative 'inline/server'
+require_relative '../coder'
+require_relative 'generic'
+
+require 'async/idler'
 
 module Async
 	module Job
 		module Processor
-			module Inline
-				def self.new(delegate)
-					return Server.new(delegate)
+			class Inline < Generic
+				def initialize(delegate, parent: nil)
+					super(delegate)
+					
+					@parent = parent || Async::Idler.new
+				end
+				
+				def call(job)
+					scheduled_at = Coder::Time(job["scheduled_at"])
+					
+					@parent.async do
+						if scheduled_at
+							sleep(scheduled_at - Time.now)
+						end
+						
+						@delegate.call(job)
+					rescue => error
+						Console.error(self, error)
+					end
+				end
+				
+				def start
+					@delegate&.start
+				end
+				
+				def stop
+					@delegate&.stop
 				end
 			end
 		end
